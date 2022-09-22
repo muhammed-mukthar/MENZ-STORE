7032a01cdecfdaeb400bc012082164985226f8a9
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-09-22 19:29:11 +0530

    some updates done

--- app.js
+++ app.js
@@ -61,19 +61,19 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   let adminlog=req.session.adminlog;
-//   let userlog= req.session.userlogin
-//   res.render('include/404',{adminlog,userlog})
+  // render the error page
+  res.status(err.status || 500);
+  let adminlog=req.session.adminlog;
+  let userlog= req.session.userlogin
+  res.render('include/404',{adminlog,userlog})
   
-// });
+});
 
 
 
